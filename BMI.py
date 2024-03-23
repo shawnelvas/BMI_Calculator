e4ca3d97ceea296ecace243058901663d2e4779a
Shawnel <104786776+shawnelvas@users.noreply.github.com>
2024-03-23 17:14:09 +0530

    Update BMI.py
    added fstring to reduce type conversion

--- BMI.py
+++ BMI.py
@@ -5,15 +5,15 @@ weight = int(input("Enter your weight in kilograms e.g., 72: "))
 
 
 #BMI Calculator and its logic
-
 Bmi = weight / (height*height)
 if  Bmi < 18.5 :
-  print("\nYour BMI is "+ str(Bmi) + ", you are underweight.")
-elif  Bmi > 18.5 and Bmi < 25:
-  print("\nYour BMI is "+str(Bmi)+", you have a normal weight.")
-elif   Bmi >= 25 and Bmi < 30:
-  print("\nYour BMI is "+str(Bmi)+", you are slightly overweight.")
-elif  Bmi >= 30 and Bmi < 35:
-  print("\nYour BMI is "+str(Bmi)+", you are obese.")
-elif  Bmi >= 35:
-  print("\nYour BMI is "+str(Bmi)+", you are clinically obese.")
+  print(f"Your BMI is {Bmi}, you are underweight.")
+elif  Bmi  < 25:
+  print(f"Your BMI is {Bmi}, you have a normal weight.")
+elif   Bmi < 30:
+  print(f"Your BMI is {Bmi}, you are slightly overweight.")
+elif  Bmi < 35:
+  print(f"Your BMI is {Bmi}, you are obese.")
+else:
+  print(f"Your BMI is {Bmi}, you are clinically obese.")
+  
